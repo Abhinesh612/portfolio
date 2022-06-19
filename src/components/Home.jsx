5a8a7ea21e1b5debd20402887d3a41fadc5ef43a
Abhinesh612 <abhineshsharma612@gmail.com>
2022-06-19 12:22:19 +0530

    19/06-01

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { HiArrowNarrowRight } from 'react-icons/hi';
+import { HiOutlineGlobe } from 'react-icons/hi';
 
 const Home = () => {
+    
     return (
         <div name='home' className='bg-[#0a192f] h-screen w-full'>
             {/* section */}
@@ -11,13 +12,13 @@ const Home = () => {
                     Abhinesh Sharma
                 </h1>
                 <p className='py-4 max-w-[700px] text-amber-100'>
-                    I'm Abhinesh Sharma. I am collage student from India who loves Computer and Mathamatics. But the most inportant I am a man of culture.
+                    I'm Abhinesh Sharma. I am collage student from India who loves Computer and Mathamatics. But the most important I am a man of culture.
                 </p>
                 <div> <a href='https://github.com/Abhinesh612' target='_blank'>
-                    <button className='test-amber-100 group px-6 py-3 my-2 flex items-center hover:bg-rose-400 bg-amber-400'>
+                    <button className='test-amber-100 group px-6 py-3 my-2 flex items-center hover:bg-rose-400 bg-amber-400 rounded-md'>
                         Github
                         <span className='group-hover:-rotate-180 ml-2 duration-500'>
-                            <HiArrowNarrowRight className='ml-3' />
+                            <HiOutlineGlobe className='ml-3' />
                         </span>
                     </button></a>
                 </div>
